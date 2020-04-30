77fa6ea0ea99757cd21f5073a10a0b5ac4799899
nitishsingla91 <nsingla85@gmail.com>
2020-04-30 17:03:44 +0530

    Update user.ts

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,17 +1,17 @@
 export class User
 {
     constructor(
-     firstname:string,
-	 lastname:string,
-	 businessunit:string,
-	 title:string,
-	 email:string,
-	 telephone:string,
-	 add1:string,
-	 add2:string,
-	 city:string,
-	 state:string,
-	 zip:string,
-	 country:string
+    public firstname:string,
+	  public lastname:string,
+	 public businessunit:string,
+	 public title:string,
+	public email:string,
+	public telephone:string,
+	public add1:string,
+	public add2:string,
+	 public city:string,
+	 public state:string,
+	 public zip:string,
+	 public country:string
     ){}
 }
